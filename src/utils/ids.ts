a9d3e6405b9bd8ab25822380a3655f376046d3cf
Anthony <smyalygames@gmail.com>
2022-07-14 02:45:59 +0100

    feat(database): add new database checks and ids for roles

---
+++ src/utils/ids.ts
@@ -0,0 +1,32 @@
+/*
+    Animal Rights Advocates Discord Bot
+    Copyright (C) 2022  Anthony Berg
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+const IDs = {
+  roles: {
+    nonvegan: '774763753308815400',
+    vegan: '788114978020392982',
+    activist: '730915638746546257',
+    plus: '798682625619132428',
+    vegCurious: '832656046572961803',
+    convinced: '797132019166871612',
+    trusted: '731563158011117590',
+    muted: '730924813681688596',
+  },
+};
+
+export { IDs };
