bc7c59411bd877e6d36fb7871a7a001e3b439330
Anthony <smyalygames@gmail.com>
2022-08-05 03:04:06 +0100

    fix(arabot): add options argument in ready listener

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -24,8 +24,9 @@ import { Listener } from '@sapphire/framework';
 import type { Client } from 'discord.js';
 
 export class ReadyListener extends Listener {
-  public constructor(context: Listener.Context) {
+  public constructor(context: Listener.Context, options: Listener.Options) {
     super(context, {
+      ...options,
       once: true,
       event: 'ready',
     });
