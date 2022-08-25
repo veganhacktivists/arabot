aec4367ce0844d858d156b150990ac5268ae8769
Kat <70778931+katefort@users.noreply.github.com>
2022-08-25 17:29:46 -0400

    fix info command to compile properly

--- src/commands/utils/info.ts
+++ src/commands/utils/info.ts
@@ -1,27 +1,25 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 /*
-    Animal Rights Advocates Discord Bot
-    Copyright (C) 2022  Anthony Berg, Kate Fort
+  Animal Rights Advocates Discord Bot
+  Copyright (C) 2022  Anthony Berg, Kate Fort
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 import { Command } from '@sapphire/framework';
 
 class InfoCommand extends Command {
-  argMap: Map<string, string>;
-
   public constructor(context: Command.Context, options: Command.Options) {
     super(context, {
       ...options,
@@ -34,42 +32,84 @@ class InfoCommand extends Command {
   public override registerApplicationCommands(registry: Command.Registry) {
     registry.registerChatInputCommand((builder) => builder
       .setName(this.name)
-      .setDescription(this.description))
-      .addStringOption((option) => option.setName('argument')
-        .setDescription('The argument you want to learn about')
-        .setRequired(true));
+      .setDescription(this.description)
+      .addStringOption((option) => option
+        .setName('argument')
+        .setDescription('The argument you want to learn more about')
+        .setRequired(true)
+        .addChoices(
+          {
+            name: 'natural',
+            value: 'Murder and rape are both natural. Does that mean they are okay?',
+          },
+          {
+            name: 'but I can',
+            value: 'You could also probably kill a baby if you wanted to. Does that make it right?',
+          },
+          {
+            name: 'resources',
+            value: 'Here are some resources compiled in a spreadsheet form! https://vegancheatsheet.org',
+          },
+          {
+            name: 'personal choice',
+            value: 'Is it okay for a killer to kill someone because it was his personal choice?',
+          },
+          {
+            name: 'I love animals',
+            value: 'Would you also say, “I love my children and I also beat them”?',
+          },
+          {
+            name: 'meat tastes good',
+            value: 'Is it morally acceptable for someone to kill a dog if that person likes the taste? \n As a vegan you can still experience all the tastes and consistencies you were used to, with the only difference that they are made of plants.',
+          },
+          {
+            name: 'animals eat meat',
+            value: 'Wild animals also kill each other. Does that mean it\'s morally acceptable for people to kill each other?',
+          },
+          {
+            name: 'it\'s my tradition',
+            value: 'Slavery, racial segregation and the oppression of women were once regarded as a tradition and part of culture. Is that why we should stick to these practices?',
+          },
+          {
+            name: 'it\'s my culture',
+            value: 'Slavery, racial segregation and the oppression of women were once regarded as a tradition and part of culture. Is that why we should stick to these practices?',
+          },
+          {
+            name: 'our ancestors ate meat',
+            value: 'Eating animals has helped our ancestors to survive. But do we still need to resort to eating meat to survive today?',
+          },
+          {
+            name: 'cows will overpopulate the world',
+            value: 'The world will not become completely vegan overnight, but this will instead be a gradual process over a long period of time.',
+          },
+          {
+            name: 'humans are more important',
+            value: "We don't need to choose between killing people and animals. Veganism is a way to avoid animal suffering while also improving the lives of humans and your own health.",
+          },
+          {
+            name: 'plants feel pain',
+            value: "It takes up to 16 kg of plants to produce 1 kg of meat. Additionally, we don't know that for a fact, where we can hear the screams of animals being slaughtered.",
+          },
+          {
+            name: "it's the food chain",
+            value: 'The food chain exists because of what animals need to do to surive. Do you need to eat animals to survive?\nAdditionally, every cruelty committed by man is based on the illusion of self-assigned power; be it Nazis who believed they were superior to Jews, white people who believed they were superior to black people, or people who believe they are superior to animals.',
+          },
+        )));
   }
 
   // Command run
-  public async getInfo(interaction: Command.ChatInputInteraction) {
-    const argument = interaction.options.getString('argument');
-    this.argMap = new Map<string, string>([
-      ['natural', 'Murder and rape are both natural. Does that mean they are okay?'],
-      ['but I can', 'You could also probably kill a baby if you wanted to. Does that make it right?'],
-      ['resources', 'Here are some resources compiled in a spreadsheet form! https://vegancheatsheet.org'],
-      ['personal choice', 'Is it okay for a killer to kill someone because it was his personal choice?'],
-      ['I love animals', 'Would you also say, “I love my children and I also beat them”?'],
-      ['meat tastes good', 'Is it morally acceptable for someone to kill a dog if that person likes the taste? \n As a vegan you can still experience all the tastes and consistencies you were used to, with the only difference that they are made of plants.'],
-      ['animals eat meat', 'Wild animals also kill each other. Does that mean it’s morally acceptable for people to kill each other?'],
-      ['tradition', 'Slavery, racial segregation and the oppression of women were once regarded as a tradition and part of culture. Is that why we should stick to these practices?'],
-      ['culture', 'Slavery, racial segregation and the oppression of women were once regarded as a tradition and part of culture. Is that why we should stick to these practices?'],
-      ['our ancestors ate meat', 'Eating animals has helped our ancestors to survive. But do we still need to resort to eating meat to survive today?'],
-      ['cows will overpopulate the world', 'The world will not become completely vegan overnight, but this will instead be a gradual process over a long period of time.'],
-      ['humans are more important', "We don't need to choose between killing people and animals. Veganism is a way to avoid animal suffering while also improving the lives of humans and your own health."],
-      ['plants feel pain', "It takes up to 16 kg of plants to produce 1 kg of meat. Additionally, we don't know that for a fact, where we can hear the screams of animals being slaughtered."],
-      ["it's the food chain", 'The food chain exists because of what animals need to do to survive. Do you need to eat animals to survive?\nAdditionally, every cruelty committed by man is based on the illusion of self-assigned power; be it Nazis who believed they were superior to Jews, white people who believed they were superior to black people, or people who believe they are superior to animals.'],
-
-    ]);
-
-    if (argument.length === 0 || !argMap.has(argument)) {
+  public static async getInfo(interaction: Command.ChatInputInteraction) {
+    const argy = interaction.options.getString('argument');
+    if (argy === null) {
       await interaction.reply({
-        content: 'Please enter a listed argument!',
+        content:
+          'Please choose a listed argument or talk to an activist for further questions!',
         ephemeral: true,
         fetchReply: true,
       });
     } else {
       await interaction.reply({
-        content: `${argMap.get(argument)}`,
+        content: `${argy}`,
         ephemeral: false,
         fetchReply: true,
       });
