8fb7ef4d4065ec1c71cf8b6089ef9d02f926fab4
leftyxiv <manny@aledoux.net>
2022-08-28 08:44:08 -0400

    chore: format document

--- src/commands/fun/wave.ts
+++ src/commands/fun/wave.ts
@@ -17,51 +17,59 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-    import { Command, RegisterBehavior } from '@sapphire/framework';
-    import { MessageEmbed } from 'discord.js';
-    import { Wave } from '../../utils/gifs';
+import { Command, RegisterBehavior } from '@sapphire/framework';
+import { MessageEmbed } from 'discord.js';
+import { Wave } from '../../utils/gifs';
 
-    export class WaveCommand extends Command {
-      public constructor(context: Command.Context, options: Command.Options) {
-        super(context, {
-          ...options,
-          name: 'wave',
-          description: 'Wave to a user',
-          preconditions: [['CoordinatorOnly', 'PatreonOnly']],
-        });
-      }
+export class WaveCommand extends Command {
+  public constructor(context: Command.Context, options: Command.Options) {
+    super(context, {
+      ...options,
+      name: 'wave',
+      description: 'Wave to a user',
+      preconditions: [['CoordinatorOnly', 'PatreonOnly']],
+    });
+  }
 
-      // Registers that this is a slash command
-      public override registerApplicationCommands(registry: Command.Registry) {
-        registry.registerChatInputCommand(
-          (builder) => builder
-            .setName(this.name)
-            .setDescription(this.description)
-            .addUserOption((option) => option.setName('user')
+  // Registers that this is a slash command
+  public override registerApplicationCommands(registry: Command.Registry) {
+    registry.registerChatInputCommand(
+      (builder) =>
+        builder
+          .setName(this.name)
+          .setDescription(this.description)
+          .addUserOption((option) =>
+            option
+              .setName('user')
               .setDescription('User you want to wave to')
-              .setRequired(true)),
-          {
-            behaviorWhenNotIdentical: RegisterBehavior.Overwrite,
-          },
-        );
+              .setRequired(true)
+          ),
+      {
+        behaviorWhenNotIdentical: RegisterBehavior.Overwrite,
       }
+    );
+  }
 
-      // Command run
-      public async chatInputRun(interaction: Command.ChatInputInteraction) {
-        // Get the users
-        // TODO exception handling
-        const user = interaction.options.getUser('user')!;
-        const waver = interaction.member!.user;
-        const waverGuildMember = interaction.guild!.members.cache.get(waver.id)!;
+  // Command run
+  public async chatInputRun(interaction: Command.ChatInputInteraction) {
+    // Get the users
+    // TODO exception handling
+    const user = interaction.options.getUser('user')!;
+    const waver = interaction.member!.user;
+    const waverGuildMember = interaction.guild!.members.cache.get(waver.id)!;
 
-        // Creates the embed for the wave
-        const randomWave = Wave[Math.floor(Math.random() * Wave.length)];
-        const waveEmbed = new MessageEmbed()
-          .setColor('#0099ff')
-          .setTitle(`${waverGuildMember.displayName} waves at you, hello!`)
-          .setImage(randomWave);
+    // Creates the embed for the wave
+    const randomWave = Wave[Math.floor(Math.random() * Wave.length)];
+    const waveEmbed = new MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle(`${waverGuildMember.displayName} waves at you, hello!`)
+      .setImage(randomWave);
 
-        // Send the wave
-        await interaction.reply({ content: `<@${user.id}>`, embeds: [waveEmbed], fetchReply: true });
-      }
-    }
+    // Send the wave
+    await interaction.reply({
+      content: `<@${user.id}>`,
+      embeds: [waveEmbed],
+      fetchReply: true,
+    });
+  }
+}
