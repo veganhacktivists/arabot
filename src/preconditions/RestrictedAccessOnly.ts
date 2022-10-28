7df01742bee750f4fe8f9f80145bdb51e7e68963
smyalygames <smyalygames@gmail.com>
2022-10-28 02:32:59 +0100

    feat(arabot): add restricted access precondition

---
+++ src/preconditions/RestrictedAccessOnly.ts
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+    Animal Rights Advocates Discord Bot
+    Copyright (C) 2022  Anthony Berg
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { AllFlowsPrecondition } from '@sapphire/framework';
+import type {
+  CommandInteraction,
+  ContextMenuInteraction,
+  Message,
+  GuildMember,
+} from 'discord.js';
+import IDs from '../utils/ids';
+
+class RestrictedAccessOnlyPrecondition extends AllFlowsPrecondition {
+  public override async messageRun(message: Message) {
+    // for message command
+    return this.checkRestrictedAccess(message.member!);
+  }
+
+  public override async chatInputRun(interaction: CommandInteraction) {
+    // for slash command
+    return this.checkRestrictedAccess(interaction.member! as GuildMember);
+  }
+
+  public override async contextMenuRun(interaction: ContextMenuInteraction) {
+    // for context menu command
+    return this.checkRestrictedAccess(interaction.member! as GuildMember);
+  }
+
+  private async checkRestrictedAccess(user: GuildMember) {
+    return user.roles.cache.has(IDs.roles.staff.restricted)
+      ? this.ok()
+      : this.error({ message: 'Only restricted access can run this command!' });
+  }
+}
+
+declare module '@sapphire/framework' {
+  interface Preconditions {
+    RestrictedAccessOnly: never;
+  }
+}
+
+export default RestrictedAccessOnlyPrecondition;
