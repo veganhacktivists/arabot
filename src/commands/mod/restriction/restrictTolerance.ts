51678a570117d42e5d5878d702e13c86f2371f4b
smyalygames <smyalygames@gmail.com>
2023-02-11 21:23:21 +0000

    feat(arabot): add restrict tolerance command

---
+++ src/commands/mod/restriction/restrictTolerance.ts
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+    Animal Rights Advocates Discord Bot
+    Copyright (C) 2023  Anthony Berg
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { Args, Command, RegisterBehavior } from '@sapphire/framework';
+import type { User, Message } from 'discord.js';
+import { restrictRun } from './restrict';
+
+export class RestrictToleranceCommand extends Command {
+  public constructor(context: Command.Context, options: Command.Options) {
+    super(context, {
+      ...options,
+      name: 'restricttolerance',
+      aliases: ['rt'],
+      description: 'Restricts a user for bigoted reasons',
+      preconditions: ['ModOnly'],
+    });
+  }
+
+  // Registers that this is a slash command
+  public override registerApplicationCommands(registry: Command.Registry) {
+    registry.registerChatInputCommand(
+      (builder) => builder
+        .setName(this.name)
+        .setDescription(this.description)
+        .addUserOption((option) => option.setName('user')
+          .setDescription('User to restrict')
+          .setRequired(true))
+        .addStringOption((option) => option.setName('reason')
+          .setDescription('Reason for restricting the user')
+          .setRequired(true)),
+      {
+        behaviorWhenNotIdentical: RegisterBehavior.Overwrite,
+      },
+    );
+  }
+
+  // Command run
+  public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+    // Get the arguments
+    const user = interaction.options.getUser('user', true);
+    const reason = interaction.options.getString('reason', true);
+    const mod = interaction.member;
+    const { guild } = interaction;
+
+    // Checks if all the variables are of the right type
+    if (guild === null || mod === null) {
+      await interaction.reply({
+        content: 'Error fetching user!',
+        ephemeral: true,
+        fetchReply: true,
+      });
+      return;
+    }
+
+    const info = await restrictRun(user?.id, mod.user.id, reason, guild, true);
+
+    await interaction.reply({
+      content: info.message,
+      fetchReply: true,
+    });
+  }
+
+  // Non Application Command method of banning a user
+  public async messageRun(message: Message, args: Args) {
+    // Get arguments
+    let user: User;
+    try {
+      user = await args.pick('user');
+    } catch {
+      await message.react('❌');
+      await message.reply('User was not provided!');
+      return;
+    }
+    const reason = args.finished ? null : await args.rest('string');
+    const mod = message.member;
+
+    if (reason === null) {
+      await message.react('❌');
+      await message.reply('Restrict reason was not provided!');
+      return;
+    }
+
+    if (mod === null) {
+      await message.react('❌');
+      await message.reply('Moderator not found! Try again or contact a developer!');
+      return;
+    }
+
+    const { guild } = message;
+
+    if (guild === null) {
+      await message.react('❌');
+      await message.reply('Guild not found! Try again or contact a developer!');
+      return;
+    }
+
+    const info = await restrictRun(user?.id, mod.user.id, reason, guild, true);
+
+    await message.reply(info.message);
+    await message.react(info.success ? '✅' : '❌');
+  }
+}
