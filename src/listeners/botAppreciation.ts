ba5cebc78af9738ed32c3f00ebb4c25db60e6ef1
smyalygames <smyalygames@gmail.com>
2023-02-14 14:10:05 +0000

    feat(arabot): let arabot appreciate the appreciation it gets

---
+++ src/listeners/botAppreciation.ts
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+    Animal Rights Advocates Discord Bot
+    Copyright (C) 2023  Anthony Berg
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { Listener } from '@sapphire/framework';
+import type { Message } from 'discord.js';
+
+export class BotAppreciationListener extends Listener {
+  public constructor(context: Listener.Context, options: Listener.Options) {
+    super(context, {
+      ...options,
+      event: 'messageCreate',
+    });
+  }
+
+  public async run(message: Message) {
+    const content = message.content.toLowerCase();
+    if (!content.includes('thanks arabot')
+      && !content.includes('thanks ara bot')
+      && !content.includes('thank you arabot')
+      && !content.includes('thank you ara bot')) {
+      return;
+    }
+
+    await message.react('💚');
+  }
+}
