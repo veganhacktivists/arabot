e21bf55fc706454a2e89ac8f947225eb71d1e3ed
smyalygames <smyalygames@gmail.com>
2023-02-20 00:00:44 +0000

    feat(arabot): add nvAccess on join if user is vegan

--- src/listeners/rolesJoinServer.ts
+++ src/listeners/rolesJoinServer.ts
@@ -37,6 +37,10 @@ export class RolesJoinServerListener extends Listener {
 
     const roles = await fetchRoles(member.id);
 
+    if (roles.includes(IDs.roles.vegan.vegan)) {
+      roles.push(IDs.roles.vegan.nvAccess);
+    }
+
     // Check if the user is restricted
     if (await checkActive(member.id)) {
       const section = await getSection(member.id);
