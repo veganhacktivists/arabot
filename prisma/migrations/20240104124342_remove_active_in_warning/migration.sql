77bbe97c6a4535b77124d0d8493b09b6825d48d4
Anthony Berg <smyalygames@gmail.com>
2024-01-04 12:44:52 +0000

    refactor(db): remove active column in Warning table

---
+++ prisma/migrations/20240104124342_remove_active_in_warning/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `active` on the `Warning` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Warning" DROP COLUMN "active";
