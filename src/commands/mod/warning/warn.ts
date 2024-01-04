94036984d3bead34f5d3b9160f0ab23ad7f7e535
Anthony Berg <smyalygames@gmail.com>
2024-01-04 11:27:30 +0000

    feat(arabot): update copyright for warn command

--- src/commands/mod/warning/warn.ts
+++ src/commands/mod/warning/warn.ts
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 /*
     Animal Rights Advocates Discord Bot
-    Copyright (C) 2023  Anthony Berg
+    Copyright (C) 2023, 2024  Anthony Berg
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
